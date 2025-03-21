24815b3c535d7e9f94f4e6a523fc441bf589202d
LorenzanaGilbert <gillorenzana0324@gmail.com>
2025-03-21 21:14:34 +0800

    Create userAge.js

---
+++ userAge.js
@@ -0,0 +1,19 @@
+//code3.js
+
+//To Collect the Information Using the prompt
+
+let Toddler = prompt("Enter your Toddler (under 5 years)");
+let Child = prompt("Enter your Child (5-12 years)");
+let Teeneger = prompt("Teeneger (13-19 years)");
+let Young = prompt("Young (20-35 years)");
+let Middle = prompt("Middle (36-60 years)");
+let Senior = prompt("Senior (60 years)");
+
+//Log the collected information to the console
+console.log("User Information");
+console.log("Toddler:", Toddler);
+console.log("Child:", Child);
+console.log("Teeneger:", Teeneger);
+console.log("Young:", Young);
+console.log("Middle:", Middle);
+console.log("Senior:", Senior);
