1339b8dac546783df34af76a59fcf3f7f0b49e66
LorenzanaGilbert <gillorenzana0324@gmail.com>
2025-03-21 20:34:21 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,14 @@
+//userprofile
+
+//
+let fullName = prompt("Enter your full name first and last name:");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = promprt("Enter your favorite color:");
+
+//Log the collected information to the console
+console.log("User Information")
+console.log("Full name:", fullName);
+console.log("Age:", age);
+console.log("Favorite  Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
