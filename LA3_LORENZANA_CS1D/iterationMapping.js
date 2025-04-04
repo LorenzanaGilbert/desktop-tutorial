a4d2e09b4dd7c18b6f59ebdb1a7d26c939d8a33f
LorenzanaGilbert <gillorenzana0324@gmail.com>
2025-04-04 21:39:47 +0800

    Create iterationMapping.js

---
+++ LA3_LORENZANA_CS1D/iterationMapping.js
@@ -0,0 +1,10 @@
+//Prompt the user for the max limit of loops
+
+let maxlimit = parseInt(prompt("Enter the limit of the Double Loops:));
+
+for (let i = 0; i <= maxLimit; i++) {
+  for (let j = 0; j <= maxtimit; j++) (
+    let addedValue = i + j;
+    console.log('[${i}] Added value is ${addedValue}');
+ }
+}
